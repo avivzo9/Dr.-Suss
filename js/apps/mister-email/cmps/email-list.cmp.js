5650a2e053b5d93e94931d9b3235f99826af6e9f
hadarmarom <hadarmarom1@gmail.com>
2021-02-24 18:50:08 +0200

    hadar-render email workes

--- js/apps/mister-email/cmps/email-list.cmp.js
+++ js/apps/mister-email/cmps/email-list.cmp.js
@@ -1,17 +1,46 @@
+import { emailsService } from '../services/email.service.js'
 export default {
-    // router: myRouter,
     template: `
-    <ul>
-        <li v-for="n in 4">
-            <h2>name</h2>
-            <h3>title</h3>
-            <h4>subtitle</h4>
-            <h3>time</h3>
+    <ul class="email-list">
+        <li  v-for="email in emails">
+            <h2>{{email.subject}}</h2>
+            <h3>{{email.body}}</h3>
+            <!-- <h4>{{}}</h4> -->
+            <h3>{{getTime(email.sentAt)}}</h3>
         </li>
     </ul>
     `,
+    data() {
+        return {
+            emails: []
 
+        }
+    },
+    methods: {
+        fetchEmails() {
+            emailsService.getEmails()
+                .then((emails) => {
+                    this.emails = emails
+                })
+                .catch((err) => {
+                    console.log('err:', err)
+                })
+        },
+        getTime(timestemp) {
+           var time= new Date(timestemp).toISOString().slice(0, 19).replace('T', ' ');
+            return time
+        }
+    },
+
+    computed: {
+    },
+    created() {
+        this.fetchEmails()
+        setTimeout(() => {
+            console.log(this.emails);
+        }, 1000)
+    },
     components: {
-   
+        emailsService
     },
 };
--- js/apps/mister-email/services/email.service.js
+++ js/apps/mister-email/services/email.service.js
@@ -1,12 +1,16 @@
-import { utilService } from './util.service.js';
-
+// import { utilService } from '../../../services/util.service.js';
+import {storageService} from '../../../services/async-storage-service.js'
 const EMAILS_KEY = 'EMAILS';
 
 export const emailsService = {
-   
+    getEmails,
+    getNextEmailId,
+    save,
+    getById,
+    remove,
 };
 
-var gBooks = [
+var gEmails = [
     {subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594},
     {subject: 'hey?', body: 'lollll!', isRead: true, sentAt : 1551133930589},
     {subject: 'Wassap?', body: 'im coollll', isRead: false, sentAt : 1551133830594},
@@ -17,83 +21,35 @@ var gBooks = [
     {subject: 'ad matai', body: 'Pick up!', isRead: true, sentAt : 1558332430594},
 ];
 
-// function query() {
-//     return getBooks();
-// }
+function getEmails() {
+    return storageService.query(EMAILS_KEY).then((emails) => {
+        if (!emails || !emails.length) {
+            return storageService.postMany(EMAILS_KEY, gEmails);
+        }
+         return emails;
+    });
+}
 
-// function getBooks() {
-//     return storageService.query(BOOKS_KEY).then((books) => {
-//         if (!books || !books.length) {
-//             gBooks.forEach((book) => (book.reviews = []));
-//             return storageService.postMany(BOOKS_KEY, gBooks);
-//         }
-//         return books;
-//     });
-// }
+function remove(emailId) {
+    return storageService.remove(EMAILS_KEY, emailId);
+}
+function getById(id) {
+    return storageService.get(EMAILS_KEY, id);
+}
 
-// function remove(bookId) {
-//     console.log(' Remove bookId:', bookId);
-//     return storageService.remove(BOOKS_KEY, bookId);
-// }
-// function getById(id) {
-//     return storageService.get(BOOKS_KEY, id);
-// }
+function save(email) {
+    if (email.id) {
+        return storageService.put(EMAILS_KEY, email);
+    } else {
+        return storageService.post(EMAILS_KEY, email);
+    }
+}
 
-// function save(book) {
-//     if (book.id) {
-//         return storageService.put(BOOKS_KEY, book);
-//     } else {
-//         return storageService.post(BOOKS_KEY, book);
-//     }
-// }
-
-// function addReview(bookId, review) {
-//     return getById(bookId).then((book) => {
-//         console.log('book433:', book);
-//         book.reviews.push(review);
-//         return storageService.put(BOOKS_KEY, book);
-//     });
-// }
-
-// function ask(x) {
-//     return axios
-//         .get(x)
-//         .then((res) => {
-//             // console.log('Service Got Res:', res);
-//             return res.data;
-//         })
-//         .catch((err) => {
-//             console.log('Service got Error:', err);
-//         });
-// }
-
-// function addGoogleBook(googleBook) {
-//     const newBook = {
-//         id: googleBook.id,
-//         title: googleBook.volumeInfo.title,
-//         subtitle: googleBook.searchInfo.textSnippet,
-//         authors: googleBook.volumeInfo.authors[0],
-//         publishedDate: googleBook.volumeInfo.publishedDate,
-//         description: googleBook.volumeInfo.description,
-//         pageCount: googleBook.volumeInfo.pageCount,
-//         categories: googleBook.volumeInfo.categories[0],
-//         thumbnail: googleBook.volumeInfo.imageLinks.thumbnail,
-//         language: googleBook.volumeInfo.language,
-//         listPrice: {
-//             amount: 20,
-//             currencyCode: googleBook.saleInfo.country,
-//             isOnSale: utilService.randomBool(),
-//         },
-//     };
-//     // gBooks.push(newBook)
-//     return storageService.post(BOOKS_KEY, newBook);
-// }
-
-// function getNextBookId(bookId) {
-//     const books = gBooks;
-//     var bookIdx = books.findIndex((book) => {
-//         return book.id === bookId;
-//     });
-//     const nextBookIdx = bookIdx + 1;
-//     return books[nextBookIdx].id;
-// }
+function getNextEmailId(emailId) {
+    const emails = gEmails;
+    var emailIdx = emails.findIndex((email) => {
+        return email.id === emailId;
+    });
+    const nextEmailIdx = emailIdx + 1;
+    return emails[nextEmailIdx].id;
+}

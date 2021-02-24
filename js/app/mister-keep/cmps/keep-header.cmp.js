a39bcb538657f3c36a823c6025714f094bbcd7ee
Aviv Zohar <avivzo9@gmail.com>
2021-02-24 14:44:06 +0200

    keep-notes Added by Aviv

---
+++ js/app/mister-keep/cmps/keep-header.cmp.js
@@ -0,0 +1,15 @@
+export default {
+    template: `
+    <section class="keep-header-container">
+        <div class="keep-header">
+            <h1>Keep</h1>
+            <input type="search" placeholder="Search note">
+            <ul class="pinned-notes">
+                <li>Pinned notes</li>
+                <li>Unpinned notes</li>
+            </ul>
+            <button class="app-picker">☰</button>
+        </div>
+    </section>
+    `
+}
--- js/app/mister-keep/services/keep.service.js
+++ js/app/mister-keep/services/keep.service.js
@@ -1,1 +1,44 @@
-// console.log();
+import { storageService } from '../../../services/async-storage-service.js'
+
+const KEY = 'notes'
+
+export const keepService = {
+    addBook,
+    query
+}
+
+var gNotes = [{
+        id: storageService.makeId(),
+        header: 'CSS',
+        text: 'Do some work on CSS',
+        color: 'blue'
+    },
+    {
+        id: storageService.makeId(),
+        header: 'JS',
+        text: 'Make ToDo List',
+        color: 'green'
+    },
+    {
+        id: storageService.makeId(),
+        header: 'HTML',
+        text: 'HTML is useless',
+        color: 'red'
+    },
+]
+
+function addBook(note) {}
+
+function query() {
+    return storageService.query(KEY)
+        .then(notes => {
+            if (!notes || !notes.length) {
+                _saveToStorage(KEY, gNotes)
+                return gNotes
+            } else return notes;
+        })
+}
+
+function _saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value) || null);
+}

f1764b86adab5e90142905313a0abb0d2dc599a5
Aviv Zohar <avivzo9@gmail.com>
2021-02-25 17:09:51 +0200

    CSS: app header, JS: classes, By Aviv Zohar

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,12 +1,12 @@
 export default {
     template: `
-        <section>
-            <div>
-                <h1>Dr. Suss</h1>
-                <nav>
-                <router-link to="/email">Email app</router-link>
-                <router-link to="/keep">Miss Keep</router-link>
-                <router-link to="/miss-book">Miss Book</router-link>
+        <section class="app-header-container container">
+            <div class="app-header-sub-container sub-container">
+                <h1>dr. seuss</h1>
+                <nav class="nav-content flex">
+                    <router-link class="router-link" to="/email">Email app</router-link>
+                    <router-link class="router-link" to="/keep">Miss Keep</router-link>
+                    <router-link class="router-link" to="/miss-book">Miss Book</router-link>
                 </nav>
             </div>
         </section>
